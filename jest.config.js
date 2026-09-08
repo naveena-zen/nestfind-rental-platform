827d6cb27a2fa95a5b4dc720191e21bca4fa778c
Naveena D <naveenaa.dhanasekaran@gmail.com>
2026-09-08 19:56:27 +0530

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  detectOpenHandles: true,
+  testTimeout: 60000,
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        strict: false,
+      },
+    },
+  },
+};
+
